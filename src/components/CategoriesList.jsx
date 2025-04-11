24c25a95b80cfedadceff28d4d4264571177bb9b
Ina <samanaviciute@gmail.com>
2025-04-11 13:25:22 +0300

    Corrected categories path

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -4,7 +4,7 @@ import Category from "./Category";
 
 export default function CategoriesList(){
     const {data: categories, loading, error} = useFetch("https://gist.githubusercontent.com/InaSG/9b3254adff6be8f79a01ad596833b288/raw/2d00eb329cc55b014c3ad37715d46588e9531741/categories.json", "category", "fileName");
-    console.log(categories);
+    console.log(categories, loading, error);
     return(
         <div className="categories-list">
             <DataHandler loading={loading} error={error}>
@@ -13,8 +13,7 @@ export default function CategoriesList(){
                         <Category
                         key={category.id}
                         title={category.title}
-                        imgSrc="src\assets\images\category\bonsai.svg"
-                        // imgSrc={category.imgSrc}
+                        imgSrc={category.imgSrc}
                         />
                     ))
                 }
--- src/hooks/useFech.js
+++ src/hooks/useFech.js
@@ -1,48 +1,52 @@
 import { useEffect, useState } from "react";
-
-export default function useFetch (url, folder, apiImg) {
-const [data, setData] = useState([]);
-const [loading, setLoading] = useState(true);
-const [error, setError] = useState(null);
-
-    useEffect (
-         () => {
-            const fetchData = async () => {
-                setLoading(true);
-                setError(null);
-                try {
-                    const response = await fetch(url);
-                    if(!response.ok) {
-                        throw new error ("Failed to retrieve data from API");
-                    }
-                    const data = await response.json();
-                    const dataImg = await Promise.all(
-                        data.map(async (elem) => {
-                            let imgPath;
-                            try{
-                                imgPath = await import(`../assets/images/${folder}/${elem[apiImg]}`)
-                            } catch (error){
-                            }
-                            return {
-                                ...elem, 
-                                imgSrc: imgPath.default
-                            };
-                        })
-                    );
-                    // await new Promise ((resolve) => 
-                    // setTimeout(
-                    //     resolve, 5000
-                    // ));
-                    setData(dataImg);
-                }
-                catch(error){
-                    setError(error.message);
-                }
-                finally {
-                    setLoading(false);
-                }
+ 
+export default function useFetch(url, folder, apiImg) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+ 
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("failed to retrieve from api");
+        }
+ 
+        const jsonData = await response.json();
+        const dataImages = await Promise.all(
+          jsonData.map(async (elem) => {
+            let imgSrc = "";
+            try {
+              imgSrc = new URL(
+                `../assets/images/${folder}/${elem[apiImg]}`,
+                import.meta.url
+              ).href;
+            } catch (err) {
+              console.warn("Nepavyko įkelti paveikslėlio:", elem[apiImg]);
+            }
+ 
+            return {
+              ...elem,
+              imgSrc,
             };
-            fetchData();
-         }, [url, folder, apiImg]);
-         return{data, loading, error};
+          })
+        );
+ 
+        setData(dataImages);
+        console.log(" Duomenys:", dataImages);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+ 
+    fetchData();
+  }, [url, folder, apiImg]);
+ 
+  return { data, loading, error };
 }
